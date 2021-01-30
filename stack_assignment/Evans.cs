8a82ab43e7b6fc17b2810c1ea528f00fbacc4c67
evans kelechi <evanskcee@gmail.com>
2021-01-30 05:58:47 -0800

    Add project files.

---
+++ stack_assignment/Evans.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stack_assignment
+{
+    public class Evans
+    {
+        private object[] array;
+        private int top;
+
+        public Evans(int size)
+        {
+            array = new object[size];
+            top = -1;
+        }
+
+       /* public void checker(object user_input)
+        {
+            if (array.Length > user_input)
+            {
+
+            }
+        }*/
+        public void push(object item)
+        {
+            if (top <= array.Length - 1)
+            {
+                top++;
+                array[top] = item;
+            }
+            else
+                throw new Exception("stack is empty..");
+        }
+
+        public object Pop()
+        {
+            if (top > -1)
+            {
+                object item = array[top];
+                top--;
+                return item;
+            }
+            else
+                throw new Exception("stack is empty....");
+        }
+
+        public object Peek()
+        {
+            if (top > -1)
+            {
+                object item = array[top];
+                return item;
+            }
+            else
+                throw new Exception("stack is empty.....");
+        }
+
+        public void Clear()
+        {
+            top = -1;
+        }
+
+        public void Print()
+        {
+            for (var i = top; i >= 0; i++)
+            {
+                Console.WriteLine("the stack contains {0}", array[i]);
+            }
+        }
+
+    }
+
+}
---
+++ stack_assignment/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace stack_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("how many object do you want in this stack? ");
+                var count = Convert.ToInt32(Console.ReadLine());
+
+                var cards = new Evans(count);
+
+                while (true)
+                {
+                    Console.WriteLine("you can either 'push', 'pop', 'clear', 'peek', 'print' items on this stack");
+                    Console.WriteLine();
+                    Console.WriteLine("what will you like to do: ");
+                    var action = Console.ReadLine();
+
+                    if (String.IsNullOrWhiteSpace(action))
+                    {
+                        Console.WriteLine("you can either 'push', 'pop', 'clear', 'peek', 'print' items on this stack");
+                    }
+                    else if (action == "push")
+                    {
+                        Console.WriteLine("what item do you want to push: ");
+                        var item = Console.ReadLine();
+
+                        cards.push(item);
+                    }
+                    else if (action == "pop")
+                    {
+                        cards.Pop();
+                    }
+                    else if (action == "peek")
+                    {
+                        cards.Peek();
+                    }
+                    else if (action == "clear")
+                    {
+                        cards.Clear();
+                    }
+                    else if (action == "print")
+                    {
+                        cards.Print();
+                    }
+                    else
+                        Console.WriteLine("you entered an invalid function.....");
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+   
+            
+
+
+        }
+    }
+}
